e96da409749c83024d7ef12c5cd95b7e3f5306fd
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-10-22 18:19:21 -0300

    feat: manager route

--- src/routes/manager-routes.tsx
+++ src/routes/manager-routes.tsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from "react-router";
-
-
-import { AppLayout } from "../components/app-layout";
+import { Routes, Route } from "react-router-dom";
+import { Refund } from "../pages/refund";
 import { Dashboard } from "../pages/dashboard";
+import { AppLayout } from "../components/app-layout";
 
 export function ManagerRoutes() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/refund/:id" element={<Refund />} />
       </Route>
 
     </Routes>
